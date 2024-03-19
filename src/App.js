2f5e98ef4b7a6739f5db57617c4ba5598c34ada6
AKSHAY BISHT <akshay.bisht.dev@gmail.com>
2024-03-19 14:50:21 +0530

    Deleted some extra files and updated public file and set favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CartPage from "./pages/Cart";
